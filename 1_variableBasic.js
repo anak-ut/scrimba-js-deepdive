4a2b928aca3e5eb3c639cb6484f37fa82f984685
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-01 22:43:49 +0700

    catatan tentang global objek

--- 1_variableBasic.js
+++ 1_variableBasic.js
@@ -3,3 +3,11 @@ let sureName = 'barger';
 let number = 'one thousand';
 
 console.log(`${firstName} , ${sureName} , ${number}`)
+
+// --- better code dengan strict mode ---
+
+// global object = object able to access any where
+// global object in browser --> window
+// console.log(window);
+
+// global object in server --> global
