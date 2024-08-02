b73a22d183d96b28e4f248341cdd585c8205c55a
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-02 12:53:09 +0700

    hoisting is var behaviour

--- 1_variableBasic.js
+++ 1_variableBasic.js
@@ -1,3 +1,4 @@
+'use strict';
 let firstName = 'reed'; // camelcase
 let sureName = 'barger';
 let number = 'one thousand';
@@ -6,8 +7,27 @@ console.log(`${firstName} , ${sureName} , ${number}`)
 
 // --- better code dengan strict mode ---
 
-// global object = object able to access any where
+// global object = object able to be accessed any where
 // global object in browser --> window
 // console.log(window);
 
-// global object in server --> global
+// global object in server/nodejs --> global
+
+// to globally preferenced (generally any env) to global object, just globalThis instead
+// console.log(globalThis);
+
+
+//message = 'hello';
+let message = 'hellooo'
+// console.log(message); // strict mode 'on', message harus di declare dg keyword
+
+// ada 2 mode js berjalan, sloppy(normal) & strict mode
+// letakkan :
+// 'use strict'; di awal kode
+
+// learn hoisting
+//var warna;
+console.log(warna);
+// var warna; output: undefined we call it hosting
+let warna; // output : referenceError
+warna = 'kuning'
