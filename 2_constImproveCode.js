a17c4643098eb27d2c24c5050c85b2cde9e31b18
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-03 21:13:28 +0700

    importance of const

---
+++ 2_constImproveCode.js
@@ -0,0 +1,15 @@
+// const - restriction make code readable
+// must initialized skalian assign
+
+const age =26;
+// age =27; const gbs di re-assign
+console.log(age);
+
+const oriPrice = 50;
+const percentOff = 20;
+const salesPrice = oriPrice * (percentOff/100);
+// var salesPrice = oriPrice * (percentOff/100);
+
+// salesPrice = 3000; //resiko pake var. nilai bs ditimpa
+// diamankan dg const
+console.log(salesPrice);
