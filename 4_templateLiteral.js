4a78c7797f3173701a84257bf95178a8a3faaa86
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-04 23:09:03 +0700

    ngerjain challenge dari bapak Scrmba

--- 4_templateLiteral.js
+++ 4_templateLiteral.js
@@ -5,3 +5,16 @@ console.log(message);
 const weight = 150;
 const moonWeight = `your weight is ${weight * 0.165} pounds on the moon`;
 console.log(moonWeight);
+
+// bikin multiple line strings
+let paragraps = `saya adalah
+seorang
+kapiten`;
+
+console.log(paragraps);
+
+// --- challenges ---
+const mySon = 'luke';
+const parentalStatus = 'father';
+const messages = `${mySon}, i am your ${parentalStatus}`;
+console.log(messages);
