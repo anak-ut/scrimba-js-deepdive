c35a743501f0452ef9ab73c60f544730b7d8f258
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-04 22:58:22 +0700

    bermain dengan template literal

---
+++ 4_templateLiteral.js
@@ -0,0 +1,7 @@
+let userName = 'jondoee'
+let message = `hi ${userName} , how are you ?`
+console.log(message);
+
+const weight = 150;
+const moonWeight = `your weight is ${weight * 0.165} pounds on the moon`;
+console.log(moonWeight);
