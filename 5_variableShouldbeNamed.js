e16c3a33c10d0cabde34011cab7326f967d1a2f0
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-05 17:55:25 +0700

    how to naming variables in JS world

---
+++ 5_variableShouldbeNamed.js
@@ -0,0 +1,15 @@
+// variabel itu case sensitif
+// name !== Name !== NAME
+// nama variabel sebaiknya self descriptive variabel itu sendiri
+// jika variabel berisi boolean, sebaiknya nama variabelnya diawali is atau has
+// variabel di javascript menggunakan camel case
+//hanya huruf awal kata pertama yang lower. selebhnya uppercase.
+// contoh ;
+let nomorBangku = 121;
+let stokSambal = 10;
+let isGoblok = true;
+
+// sedangkan penamaan variabel yg tidak ingin diasign ulang nilainya (konstan)
+// menggunakan nama var kapital seluruhnya
+// disebut identifier
+const MODAL_KOTOR = 12000;
