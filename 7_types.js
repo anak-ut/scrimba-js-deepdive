aec44b2595f7fce24ef3fe8caaf39a551e1568ff
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-05 21:13:05 +0700

    doing challenges 3 parts

--- 7_types.js
+++ 7_types.js
@@ -8,9 +8,57 @@ console.log(typeof message); // output : string
 console.log(typeof 12); // output : number
 console.log(typeof true); // output : boolean
 console.log(typeof globalThis); // output : object
+console.log(Boolean('haha')); // output: true
 
 // saatnya convert data type to another
-// - eksplisit type conversion (kita yang ubah coy) :
+// - eksplisit type conversion (kita yang ubah nyuruh JS coy) :
 // - implisit type conversion( JS does it automatic)/ coercion :
 
 console.log(typeof(String(12))); // number to string < eksplisit>
+console.log('1' + 1); // coercion
+console.log('1' * '9'); //coercion
+console.log('1' * 9); //coercion
+console.log(9 * '6'); //coercion
+console.log(9 * true); // coercion
+console.log(99 * false); //coercion
+
+// falsy --> false, 0, '', null, undefined, NaN
+
+if (true) {
+    console.log('truthy')
+} else {
+    console.log('falsy')
+}
+
+// tips : jangan lakukan direct comparison di if statement
+const userName = '';
+if (!userName){
+    console.log('no user found');
+}
+
+// use triple equals === strict data type
+
+// convert to real boolean if needed --> Boolean()
+
+console.log('--- challenges ---');
+/* chalenges 1:
+- true
+- true
+- false
+- false
+- false
+- false
+ */
+
+/* challenges 2:
+- true
+- false
+- true
+- false
+- true
+- true
+ */
+
+/* -- challenges 3:
+' ', 0, undefined, NaN, null
+ */
