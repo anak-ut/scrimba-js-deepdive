44788d206b2fe7ba20dbf4c77333df3ef2d3ded0
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-05 21:58:17 +0700

    mengerjakan ternary challenges

---
+++ 8_ternaryChallenges.js
@@ -0,0 +1,13 @@
+// challenges 1
+const isAuth = true;
+let shopChart;
+
+shopChart = isAuth? 'iPad' : '';
+console.log(`challenges 1 --- shopchart : ${shopChart}`);
+console.log(' ----------------------------------- ');
+
+const umr = 20;
+let bolehAkses;
+
+bolehAkses = umr >= 17 ? true : false ;
+console.log(`challenges 2 -- boleh akses: ${bolehAkses}`);
