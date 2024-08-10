a07bfb431e582fb9f45d2b2410f6eb9e04b605b1
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-10 15:23:37 +0700

    pake default parameter agar tidak liar hasilnya

---
+++ 12_defaultParam.js
@@ -0,0 +1,7 @@
+function toFahrenheit(celcius,decimal = 1){
+    let fahrenheit = celcius * 1.8 + 32;
+    return Number(fahrenheit.toFixed(decimal));
+}
+
+let hasil = toFahrenheit(21);
+console.log(hasil)
