3e4e660db0bf6f88e5a1ddd9f723f15d17f77e11
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-12 21:51:18 +0700

    mendalami callback

--- 13_arrowFunct.js
+++ 13_arrowFunct.js
@@ -22,4 +22,47 @@ const kapital = (nama) => `${nama.charAt(0).toUpperCase()}${nama.slice(1)}`
     // setelah parameter, kasih ' => '
     // arrow func punya implicit return. jadi curly braces dan return bs dihapus
 
-console.log(`versi arrow func: ${kapital(username)}`)
+console.log(`versi arrow func: ${kapital(username)}`);
+
+// mari kita per-rumit dengan bermain callback sederhana
+// sbg catatan, fungsi bisa digunakan seperti value/ parameter (disebut callback)
+
+// kita ingin buat fungsi greetUser dengan nyomot fungsi kapital juga
+
+// buat fungsi greet dg fungsi biasa
+
+function greetingUser(nama,callback){ // fungsi callback
+    return callback(kapital(nama));
+}
+
+const hasil = greetingUser(username, (name)=> `hasil callback : hi there, ${name}`);
+
+console.log(hasil);
+
+// tambahan:
+// callback adalah fungsi yang akan dijadikan nilai/parameter pada fungsi lain(fungsi utama)
+// fungsi utama : menerima callback
+// kode pada javascript normalnya dijalankan secara sinkronus
+// dijalankan dari atas kebawah ( baris per baris);
+// jika ada salah satu kode saja yang tersendat, akan menyebabkan blocking.
+// jika error, makan kode dihentikan paksa, padahal kode dibawahnya belum selesai
+// maka dari itu digunakan asynch callback agar kode dijalankan terus sembari menunggu kode yang berjalan lama
+// dieksekusi
+
+// contoh async
+function mainFunction() {
+    console.log("Mulai");
+
+    setTimeout(function callback() {
+        console.log("Callback dieksekusi");
+    }, 3000);
+
+    console.log("Akhir");
+}
+
+mainFunction();
+/* output :
+Mulai
+Akhir
+callback dieksekusi
+*/
