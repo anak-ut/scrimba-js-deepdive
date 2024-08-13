b9e10301fed379df5436303f810f817603c6c410
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-13 19:23:26 +0700

    nambah subbab penjelasan bagus tentang expression dan statement

---
+++ 1a_statVSexpression.js
@@ -0,0 +1,17 @@
+// source : https://www.joshwcomeau.com/javascript/statements-vs-expressions/
+
+// expression :
+// JS code produces a value
+// contoh :
+1;
+'helo duniak';
+1==2;
+3*4;
+
+// statement :
+// perintah pada komputer utk melakukan sesuatu
+let hi = 'halo' ; // memerintah komputer utk buat variabel hi berisi value 'halo'
+console.log(hi);
+if (10 < 100){
+    console.log(`hehe kecil bos`)
+}
