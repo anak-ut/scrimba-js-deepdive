0be5a2b214ca0c2418ba01650a3e683dafe83816
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-14 18:28:03 +0700

    nomor 2 syulit. kuncinya adalah merubah closure/  fungsi didalam banget jadi arrow. jadi arrow funct di dalam arrow funct jg

--- 13_arrowFunct_challenges.js
+++ 13_arrowFunct_challenges.js
@@ -4,7 +4,7 @@
 //     return `each person pay ${amount / numPeople}`;
 // }
 
-let splitBill = (amount, numPeople)=> `each person pays ${amount/numPeople}`
+let splitBill = (amount, numPeople) => `each person pays ${amount/numPeople}`
 
 console.log(splitBill(10,2));
 console.log(splitBill(10,4));
@@ -14,15 +14,25 @@ console.log(' --- --- ---');
 
 // challenge 2
 
-function countdown(startingNumber, step){
-    let countFromNum = startingNumber + step;
-    return function decrease(){
-        countFromNum -= step;
-        return countFromNum;
-    }
+// function countdown(start, step){
+//     let countFromNum = start + step;
+//     return function decrease(){
+//         countFrom -= step;
+//         return countFrom;
+//     }
+// }
+
+
+
+let countdown = (start, step) => {
+    let countFrom = start + step;
+    return () => //{
+        countFrom -= step;
+        //return countFrom;
+    //}
 }
 
-let counting = countdown(20,2);
+let counting = countdown(20,2)
 console.log(counting());
 console.log(counting());
 console.log(counting());
