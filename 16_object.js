3d0dc56b9205d5d00d574861a6e001e8f885fd82
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-18 13:55:01 +0700

    bisa memasukkan variabel ke dalam objek, panggil key = nama variabel

---
+++ 16_object.js
@@ -0,0 +1,39 @@
+// objek digunakan utk memanage key value pairs
+// pada kasus menulis function didalam objek, adalah behind the scene dari sebuah methods
+// methods adalah fungsi didalam objek
+
+/*
+
+const obj = {
+sayHi : function(){
+    console.log('hi')
+},
+sayHolo : () => console.log('holo')
+}
+
+*/
+// fungsi didalam objek pada ES6 bs ditulis lbh sederhana jadi
+// dg hapus keyword function
+const obj = {
+    sayHi() {
+        console.log('hi')
+    }
+}
+obj.sayHi(); // output : hi
+
+/*
+
+contoh method yang umum dipakai
+
+window.alert() atau biasa dipanggil alert()
+ */
+const blue = '#00f'
+
+const colorPalette = {
+    yellow: '#ff0',
+    blue,
+    orange: 'f60'
+}
+
+// value diambil dari const blue, maka tidak perlu value, ckup key saja.
+console.log(colorPalette);
