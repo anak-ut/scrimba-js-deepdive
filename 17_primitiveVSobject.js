5305ba809522f9d68f8d7e613188db3943856ac5
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-18 18:25:19 +0700

    akses key dengan dot dan bracket

--- 17_primitiveVSobject.js
+++ 17_primitiveVSobject.js
@@ -26,3 +26,34 @@ console.log('value5 :', value5);
 console.log('value3 :', value3);
 // value5 copy reference value3
 // jadi apabila dirubah value5, akan pengaruh jg ke value3
+// sifat objek ini keren tp kita tidak ingin menggunakannya.
+
+// cara memodif objek
+// nb : objek keys adalah string
+
+const colors = {
+    blue: 'f00',
+    orange: '#f60'
+}
+
+// add red colors
+colors.red = '#f00'; // cara 1
+colors.abu = '#001';
+colors['pink'] = '#123' // cara 2
+colors['coklat'] = '#heh'
+
+console.log(colors);
+console.log(colors.blue); // accept key
+console.log(colors['blue']); // accept string
+
+// hapus value key
+
+delete colors.abu;
+delete colors['red'];
+console.log(colors);
+
+function getColor(key){
+    return colors[key]; // not  colors.key
+}
+
+console.log(getColor('orange'));
