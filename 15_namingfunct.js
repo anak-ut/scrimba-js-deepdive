5098534b7e07aa504e2d5d30f28a817b6b3cf691
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-18 13:12:44 +0700

    basic rules

---
+++ 15_namingfunct.js
@@ -0,0 +1,12 @@
+// layaknya menamai variabel, menamai fungsi jg perlu ada konvensinya
+// fungsi - actions
+// contoh :
+
+// create a todo
+function createTodo() {}
+// update a todo
+function updateTodo() {}
+// check off todo
+function checkCompleteTodo() {}
+// delete todo
+function deleteTodo() {}
