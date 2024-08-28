6a6d318d08625bb703dd42d2b96df5ef199f594d
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-29 01:01:15 +0700

    mengkonversi obj ke map

--- 20_maps.js
+++ 20_maps.js
@@ -37,3 +37,53 @@ mapBaru.forEach((key,value) => {
     console.log(key,value);
 });
 
+// misal kita ingin membuat map dengan key nya adalah info user
+// dan key nya adalah password
+
+const user1 = { nama : 'kontoee'};
+const user2 = { nama : 'kwontoeeee'};
+
+// dan masing2 secret key(password)
+const secretKey1 = 'dfdfedfdd';
+const secretKey2 = 'hhfjdhfjdhfd';
+
+// untuk menggabungkannya dengan cara dimasukkan ke dalam Map
+const secretMap = new Map([
+[user1, secretKey1],
+[user2, secretKey2]
+]);
+
+// cara akses nilai map
+// misal ingin lihat
+
+const keys1 = secretMap.get(user1);
+console.log(keys1);
+
+// kita punya alternatif map yang punya fitur garbage collector
+// penggunaan map dengan membesarnya objeks,akan memakan banyak memori
+// adalah WeakMap
+// cara pakai sama dengan new Map() -> new WeakMap();
+// WeakMap hanya menerima objek as key
+const SecretWeakMap = new WeakMap([
+    [user1, secretKey1],
+    [user2, secretKey2]
+]);
+const WeaKey = SecretWeakMap.get(user1);
+console.log(`versi weaK : ${WeaKey}`);
+
+// contoh lain :
+/*
+const user99 = {
+name: 'johon',
+verified: True
+}
+merubah objek di atas menjadi map
+ */
+
+const user99Map = new Map([
+    ['name', 'johon'],
+    ['verified', true]
+])
+
+console.log(user99Map);
+console.log(user99Map.size);
