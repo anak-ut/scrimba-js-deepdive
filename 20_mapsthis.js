3302b1202c60756d7aad4b5d769426b92a2afc4f
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-04 21:11:47 +0700

    arrow func utk this  function inside function

--- 20_mapsthis.js
+++ 20_mapsthis.js
@@ -3,10 +3,31 @@ const userData = {
     title: 'JS programmer',
     getBio(){
         console.log(`User ${this.username} is a ${this.title}`);
+    },
+    askToFriend(){
+        /*
+        setTimeout(function(){
+             console.log(`berkawan yok ${this.username}`)
+         },2000);
+        */ // bentuk sebelum pake arrow utk mengakali this bisa menjangkau parent
+        setTimeout(()=> { // modif pakai arrow func
+            console.log(`berkawan yok dg si ${this.username}`);
+        },2500);
     }
 }
 
 // 'this' itu reference ke (data) objek.
 // bentuk dasarnya adalah userData.username
 // ada konsep' this' biar kita ga ngetik berulang2 nama objek
-userData.getBio();
+userData.getBio(); // output : User Reed is a JS programmer
+
+// this ini tergantung konteks,karena sifatnya dinamis
+// kalo kita panggil --> this.getBio() ga akan bs karna this nya
+// reference ke objek window, bukan ke userData
+
+userData.askToFriend(); // output: hasil undefined karna this kehilangan bindingnya
+// terjadi karna fungsi yang dipanggil ada didalam fungsi
+// this kehilangan reference ke userData
+
+// setelah dimodif jadi arrow, berhasil
+// output : berkawan yok dg si Reed
