895711b03339ee56654b0f79f84e55f6c207525f
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-11 20:20:38 +0700

    menambah element dengan push dan index

--- 21_arraySets.js
+++ 21_arraySets.js
@@ -14,12 +14,54 @@ const todos2 = {
 
 // kita tambah todos1, todos2 ke todos
 // objekTarget[key] = value;
-todos[2] = todos2;
-todos[1] = todos1;
+todos[1] = todos2;
+todos[2] = todos1;
 
 console.log(todos);
+
+/*
+output :
+{
+  dua: { text: 'do laundry', complete: false },
+  satu: { text: 'wash the dishes', complete: false }
+}
+*/
 
 // misalkan kita ingin hapus objek todos1
 // kita perlu tau kuncinya berapa
 // inilah kelemahan objek
 
+// misalkan kita mw hapus todo2
+// kita harus tau key= 1 dari todo2
+
+delete todos[1] ;
+console.log(todos);
+
+// Sekarang kita akan menggunakan array
+
+const toDo = [];
+
+toDo[0] = todos1; // first index is 0
+toDo[1] = todos2;
+const rangetoDo = [toDo.length - 1];
+console.log(rangetoDo);
+// untuk menambahkan element, kita tidak harus memasukkan dengan nilai indeks arraynya
+// kita bisa menggunakan methode push()
+const todos3 = {
+    text: ' makan nasi tok',
+    complete: false
+}
+
+toDo.push(todos3);
+console.log(toDo);
+
+/*
+output :
+[
+  { text: 'wash the dishes', complete: false },
+  { text: 'do laundry', complete: false },
+  { text: ' makan nasi tok', complete: false }
+]
+*/
+
+// kesimpulan : kita bisa menambahkan elemen ke array dengan pakai indeks atau pakai push() method
