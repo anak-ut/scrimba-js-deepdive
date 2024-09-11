96aaf0b64c1f5e800f8a288bf56fea6a68e2c053
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-11 19:41:59 +0700

    membandingkan objek dan array, ilustrasi dengan cara masukkan objek dan menghapus

--- 21_arraySets.js
+++ 21_arraySets.js
@@ -1,1 +1,25 @@
 // more flexible data type than objects = easily manipulated
+// mari kita bandingkan penggunaan object dan fleksibilitas array
+
+const todos = {};
+const todos1 = {
+    text: 'wash the dishes',
+    complete: false
+};
+
+const todos2 = {
+    text: 'do laundry',
+    complete: false
+}
+
+// kita tambah todos1, todos2 ke todos
+// objekTarget[key] = value;
+todos[2] = todos2;
+todos[1] = todos1;
+
+console.log(todos);
+
+// misalkan kita ingin hapus objek todos1
+// kita perlu tau kuncinya berapa
+// inilah kelemahan objek
+
