b86cecf91a96f9f2e5cd2b4394014151da6c5d67
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-11 21:42:35 +0700

    completed challenges 21

---
+++ 21_arraySetsChallenge.js
@@ -0,0 +1,7 @@
+const favouriteSongs = [];
+favouriteSongs.push('Hapus Aku', 'diruang rindu', 'pupus');
+const last = [favouriteSongs.length - 1]
+console.log(last)
+console.log(favouriteSongs[last]);
+favouriteSongs.pop();
+console.log(favouriteSongs);
