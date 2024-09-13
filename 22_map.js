8f5daf0003520b95055516118c5ebdc7c85380d1
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-13 21:49:22 +0700

    perbaikan kode dengan deep copy agar objek referens ga terganggu

--- 22_map.js
+++ 22_map.js
@@ -5,30 +5,10 @@ const temperaturs = [
     {degrees: 55, isRecordTemp: false}
 ];
 
-// kita misalnya akan perbaiki beberapa element
-// menggunakan .map()
-// kita akan set isRecordTemp semuanya ke true;
-// pakai .map() akan buat array baru
-// tidak akan mengganggu aray lama
-
-const newTemps = temperaturs.map(temp =>{
-    temp.isRecordTemp = true;
-    return temp;
+const mapTemps = temperaturs.map(temp=>{
+    // temp.isRecordTemp = true;
+    return {...temp, isRecordTemp :true}; // deep copy pake spread
+    // agar tidak mempengaruhi referens (temperaturs)
 })
-// console.log(newTemps); // output semua isRecordTemp = true
-
-// mari kita tambah properti baru di array
-
-const newPropsTemps = temperaturs.map((temp)=>{
-    temp.isHigh = true;
-    return temp;
-});
-// console.log(newPropsTemps);
-
-// kita set conditional
-
-const newCond = temperaturs.map(temp=>
-    temp.degrees > 56 ? {...temp,isHigh: true } : temp
-);
-
-console.log(newCond);
+console.log(temperaturs);
+console.log(mapTemps);
