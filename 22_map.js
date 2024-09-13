9dab2d2171fb2e0d2e0df1e31a3016615a92a4e6
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-13 22:13:13 +0700

    penjelasan ternary tidak menggunakan return

--- 22_map.js
+++ 22_map.js
@@ -10,5 +10,26 @@ const mapTemps = temperaturs.map(temp=>{
     return {...temp, isRecordTemp :true}; // deep copy pake spread
     // agar tidak mempengaruhi referens (temperaturs)
 })
+// console.log(temperaturs);
+// console.log(mapTemps);
+
+// mari kita tambah properti baru yaitu isHigh : true
+
+const tempIsHi = temperaturs.map(temp1=>{
+    return {...temp1, isHigh: true};
+})
+
 console.log(temperaturs);
-console.log(mapTemps);
+// console.log(tempIsHi);
+
+// merubah properti tertentu dg kondisi
+
+const condTemp = temperaturs.map(temp2=>
+    temp2.degrees > 54 ? {...temp2,isHigh: true} : {...temp2}
+) // tidak perlu blok code {} jadi dihapus kurawalnya dan tidak pake
+// return
+
+console.log(condTemp);
+// nb : ada beberapa kode yang masih pake return dan {} karena ada statement disana
+// arrow funct bisa hapus {} dan return kalau hanya expression saja
+// ternary tidak mencantumkan return karna ternary sendiri merupakan expression, bukan statement
