9fcdbf45fd49c62a9a5962e93c27ed7383833c3c
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-15 12:07:49 +0700

    belajar menggunakan filter + string method

---
+++ 23_subsetsArray.js
@@ -0,0 +1,32 @@
+const restaurants = [
+    { name: 'Cap City Diner', milesAway: 2.2 },
+    { name: 'Chop Shop', milesAway: 4.1 },
+    { name: 'Northstar Cafe', milesAway: 0.9 },
+    { name: 'City Tavern', milesAway: 0.5 },
+    { name: 'Shake Shack', milesAway: 5.3 }
+]
+
+// map method hanya cek existance. kita ingin cek isinya
+// filter method digunakan utk searching sesuatu di array
+// menggunakan filter method
+
+
+const result1 = restaurants.filter(restaurant =>restaurant.name.startsWith('C'))
+console.log(result1); // tidak merubah(mutate) array original 
+// filter diatas combine dg string method startswith utk cari name diawali c
+
+// output
+/*
+[
+  { name: 'Cap City Diner', milesAway: 2.2 },
+  { name: 'Chop Shop', milesAway: 4.1 },
+  { name: 'City Tavern', milesAway: 0.5 }
+]
+*/
+
+//kita coba cari z di nama restoran
+const cariZ = restaurants.filter(restoran=>restoran.name.startsWith('z'))
+console.log(cariZ);
+// output [] alias array kosong
+
+console.log(restaurants); // array original gak terganggu
