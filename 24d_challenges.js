8cb87799fc1685dd4155418e90e5d30d6a607317
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-18 20:59:41 +0700

    notes penting

--- 24d_challenges.js
+++ 24d_challenges.js
@@ -1,3 +1,18 @@
 const fishDishes = ['Salmon Rillettes', 'Grilled Tuna Provencal', 'Fish and Chips'];
 const meatDishes = ['Lasagna', 'Spaghetti', 'Satay Chicken Skewers'];
 
+let [chefsFishDishes, ...regularFishDishes] = fishDishes;
+console.log(chefsFishDishes);
+console.log(regularFishDishes);
+
+let [regularMeatDishes, ...chefsMeatDishes] = meatDishes;
+console.log(chefsMeatDishes);
+console.log(regularMeatDishes);
+
+console.log('--- final Results ---')
+let chefsDishes = [chefsFishDishes, ...chefsMeatDishes]; // perlu di spread karna masih mengandung array
+let regularDishes = [...regularFishDishes, regularMeatDishes]; // dispread jg karna mengandung array
+console.log(chefsDishes);
+console.log(regularDishes);
+
+// course ga selesai. Keburu habis free trial github studennya 1 bulan
