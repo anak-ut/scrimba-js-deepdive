a0beaa8036957167d372cd75877599798ac14b12
anak-ut <031317495@ecampus.ut.ac.id>
2024-09-18 20:37:34 +0700

    rest dan destruct

--- 24d_flexibleArraywithdestruct.js
+++ 24d_flexibleArraywithdestruct.js
@@ -12,8 +12,22 @@ let item3 = finalItems[2];
 console.log(item1,item2,item3);
 
 // cara amazing dg destrukturing
-const [menu1,menu2,menu3] = finalItems;
+let [menu1,menu2,menu3] = finalItems;
 console.log(menu1,menu2,menu3);
 
 // kita cetak destrukturing diatas jadi objek
 console.log({menu1}, {menu2}, {menu3}); // nama var destruct sbg key
+
+// coba kita swap menu1 = menu2
+// tidak perlu declare nilai. karna kita mw swap saja
+[menu2,menu1,menu3] = [menu1,menu2,menu3];
+console.log({menu1},{menu2},{menu3});
+
+
+// rest operator dan destructur main bersama
+
+const warna = ['kuning', 'biru', 'abu-abu'];
+// kita masukkan kuning sbg var golkar, sisanya lain lain
+const [golkar, ...lainnya] = warna;
+console.log(golkar);
+console.log(lainnya);
